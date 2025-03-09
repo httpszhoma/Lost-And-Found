6e956c7d09f0af057f44b3457c2adae6b2d8b8be
Zhomart <zturtan@gmail.com>
2025-03-09 21:04:38 +0500

    finishing Jwtservice i added token generation and validation of tokens refresh and access

--- src/main/java/zhoma/security/JwtService.java
+++ src/main/java/zhoma/security/JwtService.java
@@ -3,15 +3,24 @@ package zhoma.security;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import zhoma.exceptions.TokenInvalidException;
+import zhoma.models.User;
 import zhoma.repository.UserRepository;
 import io.jsonwebtoken.security.Keys;
 
 
 import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
 
 @Service
@@ -26,6 +35,7 @@ public class JwtService {
     @Value("${security.jwt.refresh-expiration-time}")
     private long refreshJwtExpiration;
 
+    @Getter
     private final UserRepository userRepository;
 
     public String extractUsername(String token) {
@@ -38,6 +48,7 @@ public class JwtService {
         return claimsResolver.apply(claims);
 
     }
+
 
     private Claims extractAllClaims(String token) {
         return Jwts
@@ -54,5 +65,63 @@ public class JwtService {
         return Keys.hmacShaKeyFor(keyBytes);
     }
 
+    public String generateToken(UserDetails userDetails) {
+        return generateToken(new HashMap<>(), userDetails, jwtExpiration);
+    }
+
+    public String generateToken(Map<String, Object> extraClaims, UserDetails userDetails, long expiration) {
+
+        return buildToken(extraClaims, userDetails, expiration);
+    }
+
+    private String buildToken(
+            Map<String, Object> extraClaims,
+            UserDetails userDetails,
+            long expiration) {
+        return Jwts.builder()
+                .setClaims(extraClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername())) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+
+    public String refreshAccessToken(String refreshToken) {
+        String username = extractUsername(refreshToken);
+        User user = userRepository.findByUsername(username).orElseThrow(() ->
+                new UsernameNotFoundException(username));
+        if (!isRefreshTokenValid(refreshToken, user)) {
+            throw new TokenInvalidException("Refresh token is invalid");
+        }
+        System.out.println(username);
+        System.out.println();
+        System.out.println(generateToken(user));
+        return generateToken(user);
+
+    }
+
+    public String generateRefreshToken(UserDetails userDetails) {
+        return generateToken(new HashMap<>(), userDetails, refreshJwtExpiration);
+    }
+
+    public boolean isRefreshTokenValid(String refreshToken, UserDetails userDetails) {
+        final String username = extractUsername(refreshToken);
+        return (username.equals(userDetails.getUsername())) && !isTokenExpired(refreshToken);
+    }
 
 }
